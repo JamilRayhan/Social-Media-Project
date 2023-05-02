8150f149171374f569721e973f53d54a878f8334
JamilRayhan <jamilrayhan.cse@gmail.com>
2023-05-02 18:26:04 +0600

    search init

--- App_Post/views.py
+++ App_Post/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
+from App_Login.models import UserProfile
+from django.contrib.auth.models import User
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'App_Post/home.html',context={'title':'News Feed'})
+    if request.method=='GET':
+        search = request.GET.get('search',  ' ')
+        result= User.objects.filter(username__icontains=search)
+    return render(request,'App_Post/home.html',context={'title':'News Feed','search':search,'result':result})
